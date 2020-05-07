dc9f8143ae2061bd522fb22a2b7382c8fea7abd1
bilginmrv <47225405+mervebilgin@users.noreply.github.com>
2020-05-07 10:27:51 +0300

    Add files via upload

---
+++ main24.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+// Dýþarýdan girilen N elemanlý bir dizinin aritmetik, geometrik ve harmonik ortalamasýný hesaplayan program...
+
+int main() {
+	
+	int i,n;
+    int dizi[n];
+    float ariort,geoort,harort;
+
+    ariort=0;
+    geoort=1;
+    harort=0.0;
+    
+    cout<< "Dizinin eleman sayisini giriniz: ";
+    cin>> n;
+
+    for(i=0;i<n;i++){
+    	
+    	cout<< "dizinin "<< i+1<< ". elemani: ";
+    	cin>>dizi[i];
+	}
+         
+    for(i=0;i<n;i++){
+    	ariort=ariort+dizi[i];
+        geoort=geoort*dizi[i];
+        harort=harort+1.0/dizi[i];
+    }
+    
+    ariort=ariort/n;
+    geoort=pow(geoort,(1/n));
+    harort=n/harort;
+    
+    cout<<endl;
+    
+    cout<< "Aritmetik Ortalama= "<< ariort<< endl;
+    cout<< "Geometrik Ortalama= "<< geoort<< endl;
+    cout<< "Harmonik Ortalama= "<< harort<< endl;	
+		
+	return 0;
+}
