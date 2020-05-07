17a8e51336186cc5453cabb2c9592162e2b8d680
bilginmrv <47225405+mervebilgin@users.noreply.github.com>
2020-05-07 10:38:52 +0300

    Add files via upload

---
+++ main18.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+// Dýþarýdan girilen N elemanlý bir diziyi tersine çeviren program...
+int main() {
+	int i, n;
+	int dizi[n];
+	int terscevir[n];
+	cout<< "N degerini giriniz: ";
+	cin>> n;
+	for(i=0; i<n; i++){
+		cout<< i+1<< ". elemani giriniz: ";
+		cin>> dizi[i];
+	}
+	for (int j=0; j<n; j++){
+		
+        terscevir[n-1-j]= dizi[j];
+    }
+    cout<< "Ters cevirilmis hali: "<< endl;
+    cout<< "terscevir[]= {";
+    for (int j = 0; j < n; j++){
+    	
+    	cout<< terscevir[j]<< ", ";	
+	}
+	cout<< "}";
+	return 0;
+}
