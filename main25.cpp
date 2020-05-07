14b6aec35b9f5923c3312bd3f6383f471c7fa7a3
bilginmrv <47225405+mervebilgin@users.noreply.github.com>
2020-05-07 10:22:23 +0300

    Add files via upload

---
+++ main25.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+// Bir dizideki elemanlarýn standart sapmasýný ve varyansýný bulan program...
+
+int main() {
+	
+	int i,n;
+    int dizi[n];
+    float ariort,stsapma,varyans;
+    
+    ariort=0;
+    varyans=0;
+    
+    cout<< "Dizinin eleman sayisini giriniz: ";
+    cin>> n;
+
+    for(i=0;i<n;i++){
+    	
+    	cout<< "Dizinin "<< i+1<< ". elemani: ";
+        cin>> dizi[i];
+    }
+    
+    for(i=0;i<n;i++){
+    	ariort=ariort+dizi[i];	
+	}
+    ariort=ariort/n;
+    
+    for(i=0;i<n;i++){
+    	varyans=varyans+(dizi[i]-ariort)*(dizi[i]-ariort);
+	}
+    varyans=varyans/n;
+    stsapma=sqrt(varyans);
+    
+    cout<< endl;
+    cout<< "Aritmetik Ortalama= "<< ariort<< endl;
+    cout<< "Varyans= "<< varyans<< endl;
+    cout<< "Standart Sapma= "<< stsapma <<endl;
+    
+	return 0;
+}
